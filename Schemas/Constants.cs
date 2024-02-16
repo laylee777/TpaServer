f85b34bdfe06d8b891bffc73c6081d32aff503b9
hdlee <hdlee@ivm.co.kr>
2024-02-16 11:07:21 +0900

    초기 커밋.

---
+++ Schemas/Constants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSEV.Schemas
+{
+    public enum 피씨구분
+    {
+        Server,
+        Client,
+    }
+
+    public enum 동작구분
+    {
+        Live = 0,
+        LocalTest = 2
+    }
+}
