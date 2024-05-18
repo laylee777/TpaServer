4ef6d38004c10a57b9cb56acad15127d71330dd8
KIM HYOUNG MIN <rlagud2005@naver.com>
2024-05-17 17:11:24 -0700

    ✒️Feat : Test

---
+++ Migrations/20240517230604_init.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DSEV.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "public");
+
+            migrationBuilder.CreateTable(
+                name: "inspd",
+                schema: "public",
+                columns: table => new
+                {
+                    idwdt = table.Column<DateTime>(nullable: false),
+                    iditm = table.Column<int>(nullable: false),
+                    idgrp = table.Column<int>(nullable: false),
+                    iddev = table.Column<int>(nullable: false),
+                    idcat = table.Column<int>(nullable: false),
+                    iduni = table.Column<int>(nullable: false),
+                    idstd = table.Column<decimal>(nullable: false),
+                    idmin = table.Column<decimal>(nullable: false),
+                    idmax = table.Column<decimal>(nullable: false),
+                    idoff = table.Column<decimal>(nullable: false),
+                    idcal = table.Column<decimal>(nullable: false),
+                    idmes = table.Column<decimal>(nullable: false),
+                    idval = table.Column<decimal>(nullable: false),
+                    idres = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_inspd", x => new { x.idwdt, x.iditm });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "inspl",
+                schema: "public",
+                columns: table => new
+                {
+                    ilwdt = table.Column<DateTime>(nullable: false),
+                    ilmcd = table.Column<int>(nullable: false),
+                    ilnum = table.Column<int>(nullable: false),
+                    ilres = table.Column<int>(nullable: false),
+                    ilctq = table.Column<int>(nullable: false),
+                    ilsuf = table.Column<int>(nullable: false),
+                    ilqrs = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_inspl", x => x.ilwdt);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "inspd",
+                schema: "public");
+
+            migrationBuilder.DropTable(
+                name: "inspl",
+                schema: "public");
+        }
+    }
+}
---
+++ Migrations/캘리브테이블Migrations/20240517230708_init.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DSEV.Migrations.캘리브테이블Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "public");
+
+            migrationBuilder.CreateTable(
+                name: "calibs",
+                schema: "public",
+                columns: table => new
+                {
+                    ctime = table.Column<DateTime>(nullable: false),
+                    ccame = table.Column<int>(nullable: false),
+                    cindx = table.Column<int>(nullable: false),
+                    cwidt = table.Column<decimal>(nullable: false),
+                    cheig = table.Column<decimal>(nullable: false),
+                    clent = table.Column<decimal>(nullable: false),
+                    clenb = table.Column<decimal>(nullable: false),
+                    clenl = table.Column<decimal>(nullable: false),
+                    clenr = table.Column<decimal>(nullable: false),
+                    cangt = table.Column<decimal>(nullable: false),
+                    cangb = table.Column<decimal>(nullable: false),
+                    cangl = table.Column<decimal>(nullable: false),
+                    cangr = table.Column<decimal>(nullable: false),
+                    ccalx = table.Column<decimal>(nullable: false),
+                    ccaly = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_calibs", x => x.ctime);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "calibs",
+                schema: "public");
+        }
+    }
+}
